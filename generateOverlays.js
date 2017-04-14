90a421e4a66e57257913e9b5b1f2aa4fbf5e4a31
Danverde <gre12014@byui.edu>
2017-04-14 12:20:18 -0600

    icons now appear only on hover & many other changes!

--- generateOverlays.js
+++ generateOverlays.js
@@ -1,333 +1,391 @@
 var links = [
     {
         selector: "map",
-        description: "Return ot the Museum Map",
-        image: "testIcon.jpg",
+        description: "Return to the Museum Map",
+        image: "map.jpg",
         filePath: "Museum%20Map.html"
     },
     {
         selector: "workspace1",
         description: "Go to the workspace for this room",
-        image: "testIcon.jpg",
-        filePath: "Unit%20One%20Workspace.html"
+        image: "workspace1.jpg",
+        filePath: "Section%201%20Workspace.html"
     },
     {
         selector: "workspace2",
         description: "Go to the workspace for this room",
-        image: "testIcon.jpg",
+        image: "workspace2.jpg",
         filePath: "Section%202%20Workspace.html"
     },
     {
         selector: "workspace3",
         description: "Go to the workspace for this room",
-        image: "testIcon.jpg",
+        image: "workspace3.jpg",
         filePath: "Section%203%20Workspace.html"
     },
     {
         selector: "workspace4",
         description: "Go to the workspace for this room",
-        image: "testIcon.jpg",
+        image: "workspace4.jpg",
         filePath: "Section%204%20Workspace.html"
     },
     {
-        selector: "workspace5",
+        selector: "workspace5e",
         description: "Go to the workspace for this room",
-        image: "testIcon.jpg",
+        image: "workspace5e.jpg",
+        filePath: "Section%206%20Workspace.html"
+    },
+    {
+        selector: "workspace5s",
+        description: "Go to the workspace for this room",
+        image: "workspace5s.jpg",
         filePath: "Section%205%20Workspace.html"
     },
     {
         selector: "workspace6",
         description: "Go to the workspace for this room",
-        image: "testIcon.jpg",
-        filePath: "Section%206%20Workspace.html"
+        image: "workspace6.jpg",
+        filePath: "Section%207%20Workspace.html"
     },
     {
         selector: "workspace7",
         description: "Go to the workspace for this room",
-        image: "testIcon.jpg",
-        filePath: "Section%207%20Workspace.html"
+        image: "workspace7.jpg",
+        filePath: "Section%208%20Workspace.html"
     },
     {
         selector: "workspace8",
         description: "Go to the workspace for this room",
-        image: "testIcon.jpg",
-        filePath: "Section%208%20Workspace.html"
-    },
-    {
-        selector: "workspace9",
-        description: "Go to the workspace for this room",
-        image: "testIcon.jpg",
+        image: "workspace8.jpg",
         filePath: "Section%209%20Workspace.html"
     },
     {
         selector: "room1",
         description: "1. Entrance Hall",
-        image: "testIcon.jpg",
+        image: "room1.jpg",
         filePath: "Entrance%20Hall.html"
     },
     {
         selector: "room2",
         description: "2. Becoming a Scholar-leader",
-        image: "testIcon.jpg",
+        image: "room2.jpg",
         filePath: "Becoming%20a%20Scholar-leader.html"
     },
     {
         selector: "room3",
         description: "3. Becoming a Scholar-leader II",
-        image: "testIcon.jpg",
+        image: "room3.jpg",
         filePath: "Becoming%20a%20Scholar-Leader%20II.html"
     },
     {
         selector: "room4",
-        description: "4. Defining Art",
-        image: "testIcon.jpg",
-        filePath: "LOST.HTML" //CAN'T FIND
+        description: "4. Defining Art/ Arts in the Modern Era",
+        image: "room4.jpg",
+        filePath: "Arts%20in%20the%20Modern%20Era.HTML"
     },
     {
         selector: "room5",
         description: "5. Complexities: Nudity in Art",
-        image: "testIcon.jpg",
+        image: "room5.jpg",
         filePath: "Complexities%20Nudity%20in%20Art.html"
     },
     {
         selector: "room6",
         description: "6. Arts and Propaganda",
-        image: "testIcon.jpg",
+        image: "room6.jpg",
         filePath: "Arts%20and%20Propaganda.html"
     },
     {
         selector: "room7",
         description: "7. Propaganda II",
-        image: "testIcon.jpg",
-        filePath: "LOST" //CANT FIND
+        image: "room7.jpg",
+        filePath: "Complexities%20Ambiguity%20in%20Art.html" //CHANGE NAME?
     },
     {
         selector: "room8",
         description: "8. Birth of Modernism",
-        image: "testIcon.jpg",
+        image: "room8.jpg",
         filePath: "Birth%20of%20Modernism.html"
     },
     {
         selector: "room9",
         description: "9. 20th Century Art",
-        image: "testIcon.jpg",
+        image: "room9.jpg",
         filePath: "20th%20Century%20Art.html"
     },
     {
         selector: "room10",
         description: "10. 20th Century Music",
-        image: "testIcon.jpg",
+        image: "room10.jpg",
         filePath: "20th%20Century%20Music.html"
     },
     {
         selector: "room11",
         description: "11. Perceptions of Beauty",
-        image: "testIcon.jpg",
+        image: "room11.jpg",
         filePath: "Perceptions%20of%20Beauty.html"
     },
     {
         selector: "room12",
         description: "12. WWI",
-        image: "testIcon.jpg",
-        filePath: "WWI.html" //DUPLICATES FILE EXIST
+        image: "room12.jpg",
+        filePath: "WWI.html" //DUPLICATES FILE EXIST and are the exact same!
     },
     {
         selector: "room13",
-        description: "12. Religion in the 20th Century",
-        image: "testIcon.jpg",
+        description: "13. Religion in the 20th Century",
+        image: "room13.jpg",
         filePath: "Religion.html"
     },
     {
         selector: "room14",
         description: "14. Art and Politics in the USSR",
-        image: "testIcon.jpg",
+        image: "room14.jpg",
         filePath: "Art%20and%20Politics%20in%20the%20USSR.html"
     },
     {
         selector: "room15",
         description: "15. Nationalistic Music",
-        image: "testIcon.jpg",
+        image: "room15.jpg",
         filePath: "Nationalistic%20Music.html"
     },
     {
         selector: "room16",
         description: "16. Depression and the Common Man",
-        image: "testIcon.jpg",
+        image: "room16.jpg",
         filePath: "The%20Depression%20and%20the%20Common%20Man.html"
     },
     {
         selector: "room17",
         description: "17. Nostalgia, Nationalism, and Romanticism",
-        image: "testIcon.jpg",
+        image: "room17.jpg",
         filePath: "Nationalism,%20Nostalgia%20and%20Romanticism.html"
     },
     {
         selector: "room18",
         description: "18. Harlem Renaissance",
-        image: "testIcon.jpg",
+        image: "room18.jpg",
         filePath: "Harlem%20Renaissance.html"
     },
     {
         selector: "room19",
         description: "19. Jazz",
-        image: "testIcon.jpg",
+        image: "room19.jpg",
         filePath: "Jazz.html"
     },
     {
         selector: "room20",
         description: "20. WWII",
-        image: "testIcon.jpg",
+        image: "room20.jpg",
         filePath: "WWII.html"
     },
     {
         selector: "room21",
         description: "21. Holocaust",
-        image: "testIcon.jpg",
+        image: "room21.jpg",
         filePath: "Holocaust.html"
     },
     {
         selector: "room22",
         description: "22. Film as Propaganda",
-        image: "testIcon.jpg",
+        image: "room22.jpg",
         filePath: "Film%20as%20Propaganda.html"
     },
     {
         selector: "room23",
         description: "23. Film as Art",
-        image: "testIcon.jpg",
+        image: "room23.jpg",
         filePath: "Film%20as%20Art%20Citizen%20Kane.html"
     },
     {
         selector: "room24",
         description: "24. Women in a Century of Change",
-        image: "testIcon.jpg",
+        image: "room24.jpg",
         filePath: "Women%20in%20a%20Century%20of%20Change.html"
     },
     {
         selector: "room25",
         description: "25. 20th Century Novels",
-        image: "testIcon.jpg",
+        image: "room25.jpg",
+        filePath: "20th%20Century%20Novels.html"
+    },
+    {
+        selector: "returnToMap",
+        description: "Return to the Museum",
+        image: "room25.jpg",
         filePath: "20th%20Century%20Novels.html"
     },
     {
         selector: "room26",
         description: "26. Dystopic Visions",
-        image: "testIcon.jpg",
-        filePath: "Dystopic%20Visions.html"
+        image: "room26.jpg",
+        filePath: "Dystopic%20Visions.html" //broken for some reason...
     },
     {
         selector: "room27",
         description: "27. 1984",
-        image: "testIcon.jpg",
+        image: "room27.jpg",
         filePath: "1984.html"
     },
     {
         selector: "room28",
         description: "28. Civil Rights",
-        image: "testIcon.jpg",
+        image: "room28.jpg",
         filePath: "Civil%20Rights.html"
     },
     {
         selector: "room29",
         description: "29. A Raisin in the Sun",
-        image: "testIcon.jpg",
+        image: "room29.jpg",
         filePath: "A%20Raisin%20in%20the%20Sun.html"
     },
     {
         selector: "room30",
         description: "30. The Cold War",
-        image: "testIcon.jpg",
+        image: "room30.jpg",
         filePath: "Cold%20War.html"
     },
     {
         selector: "room31",
         description: "31. Enemy of the People",
-        image: "testIcon.jpg",
-        filePath: "Emeny%20of%20the%20People.html" // MULPILE FILES POSSIBLE
+        image: "room31.jpg",
+        filePath: "Enemy%20of%20the%20People.html"
     },
     {
         selector: "room32",
         description: "32. Consumerism and the Environment",
-        image: "testIcon.jpg",
-        filePath: "Consumerism%20and%20the%20Environment.html"
+        image: "room32.jpg",
+        filePath: "Consumerism%20and%20Environment.html"
     },
     {
         selector: "room33",
         description: "33. Eastern Europe",
-        image: "testIcon.jpg",
+        image: "room33.jpg",
         filePath: "Eastern%20Europe.html"
     },
     {
         selector: "room34",
         description: "34. Propaganda Today",
-        image: "testIcon.jpg",
-        filePath: ".html" // CANNOT FIND FILE
+        image: "room34.jpg",
+        filePath: "Propaganda%20Today.html"
     },
     {
         selector: "room35",
         description: "35. Ancient as Modern",
-        image: "testIcon.jpg",
+        image: "room35.jpg",
         filePath: "Ancient%20as%20Modern.html"
     },
     {
         selector: "room36",
         description: "36. Postmodern",
-        image: "testIcon.jpg",
-        filePath: ".html" //CANNOT FIND FILE
+        image: "room36.jpg",
+        filePath: "Postmodern.html"
     },
     {
         selector: "room37",
         description: "37. Colonialism and Apartheid",
-        image: "testIcon.jpg",
+        image: "room37.jpg",
         filePath: "Colonialism%20and%20Apartheid.html"
     },
     {
         selector: "room38",
         description: "38. Art as Social Protest: Latin America",
-        image: "testIcon.jpg",
+        image: "room38.jpg",
         filePath: "Art%20as%20Social%20Protest%20Latin%20America.html"
     },
     {
         selector: "room39",
-        description: "34. Art in response to trauma",
-        image: "testIcon.jpg",
+        description: "39. Art in response to trauma",
+        image: "room39.jpg",
         filePath: "Art%20in%20Response%20to%20Trauma.html"
     },
     {
         selector: "room40",
         description: "40. Special Exhibit",
-        image: "testIcon.jpg",
+        image: "room40.jpg",
         filePath: "Special%20Exhibit.html"
     },
     {
-        selector: "2018",
+        selector: "room41",
         description: "Exit",
-        image: "testIcon.jpg",
+        image: "room41.jpg",
         filePath: "exit.html"
     },
     {
         selector: "recentWars",
         description: "Recent Wars",
-        image: "testIcon.jpg",
+        image: "recentWars.jpg",
         filePath: "Recent%20Wars.html"
+    },
+    {
+        selector: "books",
+        special: true,
+        books: [{
+                title: "1984",
+                filePath: "1984%20Group%20Room.html"
+            },
+            {
+                title: "A tree Growns in Brooklyn",
+                filePath: "A%20Tree%20Grows%20In%20Brooklyn.html"
+            },
+            {
+                title: "Bless Me, Ultima",
+                filePath: "Bless%20Me,%20Ultima.html"
+            },
+            {
+                title: "Cry, the Beloved Country",
+                filePath: "Cry,%20the%20Beloved%20Country.html"
+            },
+            {
+                title: "The Invisible Wall",
+                filePath: "The%20Invisible%20Wall.html"
+            },
+            {
+                title: "The Namesake",
+                filePath: "The%20Namesake.html"
+            },
+            {
+                title: "Their Eyes Were Watching God",
+                filePath: "Their%20Eyes%20Were%20Watching%20God.html"
+            }
+               ]
     }
 ];
 
 
 
+// adds an event listener that makes D2L icons appear on hover
+function toggleVisibility() {
+    $(".thingLinkFrame, .button").hover(function () {
+        $(".button").css("opacity", "1");
+    }, function () {
+        $(".button").css("opacity", "0");
+    });
+}
+
+
 $(document).ready(function () {
-    console.log(window.location);
-    var ou = window.location.pathname.match(/(?:enforced|home)\/(\d+)/);
-    if (ou != null) {
-        ou = ou[1];
+    toggleVisibility();
+    //    console.log(window.location);
+    var courseCode = window.location.pathname.match(/(?:enforced|home)\/(\d+.*\/)/);
+    if (courseCode != null) {
+        courseCode = courseCode[1];
     }
 
     function buildHTML(link) {
         var htmlStr = "";
 
-        htmlStr += "<a href='https://byui.brightspace.com/content/enforced/" + ou + link.filePath + "-Campus.2017.Spring.SandboxCourses-Template/' target='_parent'>";
-        htmlStr += "<img class='popupImage' src='" + link.image + "' style='height:150px'>";
-        htmlStr += "<p>" + link.description + "</p></a>";
+        // this is to generate the popup including links to the various book rooms
+        if (link.special === true) {
+            htmlStr += "<p>GROUP ROOMS:</p>";
+            link.books.forEach(function (book) {
+                htmlStr += "<a href='" + book.filePath + "'>" + book.title + "</a><br>";
+            });
+        } else {
+            htmlStr += "<a href='./" + link.filePath + "' target='_top'>";
+            htmlStr += "<img class='popupImage' src='/content/enforced/" + courseCode + "gamification/map/popupImages/" + link.image + "' style='height:150px'>";
+            htmlStr += "<p>" + link.description + "</p></a>";
+        }
 
         return htmlStr;
     }
@@ -337,7 +395,17 @@ $(document).ready(function () {
             content: buildHTML(link),
             interactive: true,
             contentAsHTML: true,
-            theme: ['tooltipster-shadow', 'tooltipster-shadow-customized']
+            theme: ['tooltipster-shadow', 'tooltipster-shadow-customized'],
+            functionReady: function (instance, helper) {
+                console.log("HELPER", helper);
+                $(".tooltipster-base").hover(function () {
+                    $(".button").css("opacity", "1");
+                }, function () {
+                    $(".button").css("opacity", "0");
+                });
+            }
         });
+        $("." + link.selector).wrap("<a href=" + link.filePath + " target='_top'></a>")
+
     });
 });
